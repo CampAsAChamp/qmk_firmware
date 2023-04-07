63b443089cc81b948cee0f010f376eb142cbd422
Nick Schneider <nickschneider101@gmail.com>
2023-04-07 08:20:47 -0700

    Formatting on rules.mk

--- keyboards/ergodox_ez/keymaps/campasachamp/rules.mk
+++ keyboards/ergodox_ez/keymaps/campasachamp/rules.mk
@@ -4,21 +4,21 @@
 # OPT_DEFS += -DLEFT_LEDS
 
 # Build Options
-BOOTMAGIC_ENABLE 	= no   	# Enable Bootmagic Lite
+BOOTMAGIC_ENABLE 	= no   	         # Enable Bootmagic Lite
 CAPS_WORD_ENABLE    = yes
-COMMAND_ENABLE   	= no   	# Commands for debug and configuration
-CONSOLE_ENABLE   	= no   	# Console for debug
-CUSTOM_MATRIX    	= lite 	# Custom matrix file for the ErgoDox EZ
+COMMAND_ENABLE   	= no   	         # Commands for debug and configuration
+CONSOLE_ENABLE   	= no   	         # Console for debug
+CUSTOM_MATRIX    	= lite 	         # Custom matrix file for the ErgoDox EZ
 DEBOUNCE_TYPE 		= sym_eager_pr
-EXTRAKEY_ENABLE  	= yes  	# Audio control and System control
-MOUSEKEY_ENABLE  	= yes  	# Mouse keys
-NKRO_ENABLE      	= yes  	# Enable N-Key Rollover
+EXTRAKEY_ENABLE  	= yes  	         # Audio control and System control
+MOUSEKEY_ENABLE  	= yes  	         # Mouse keys
+NKRO_ENABLE      	= yes  	         # Enable N-Key Rollover
 RGB_MATRIX_DRIVER 	= IS31FL3731
-RGBLIGHT_ENABLE     = no    # Only available on the Shine
+RGBLIGHT_ENABLE     = no             # Only available on the Shine
 RGB_MATRIX_ENABLE   = yes
-SWAP_HANDS_ENABLE	= no   	# Allow swapping hands of keyboard
+SWAP_HANDS_ENABLE	= no   	         # Allow swapping hands of keyboard
 TAP_DANCE_ENABLE 	= yes
-UNICODE_ENABLE   	= no   	# Unicode
+UNICODE_ENABLE   	= no   	         # Unicode
 
 # project specific files
 SRC += matrix.c \
