a1fb16b7d8f0e5c72df4436cf9e7c4aa78950b15
Nick Schneider <nickschneider101@gmail.com>
2023-05-09 10:08:00 -0700

    Add cmd tab to base layer and swap with screenshot button

--- keyboards/ergodox_ez/keymaps/campasachamp/keymap.c
+++ keyboards/ergodox_ez/keymaps/campasachamp/keymap.c
@@ -169,9 +169,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,     KC_W,     KC_E,           KC_R,           KC_T,     TD(TD_LBRACKET),       TD(TD_RBRACKET),  KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           TD(TD_PIPE),
     MO(SYMBOLS),    KC_A,     KC_S,     KC_D,           KC_F,           KC_G,                                              KC_H,           KC_J,           KC_K,           KC_L,           KC_SEMICOLON,   KC_QUOTE,
     KC_LSFT,        KC_Z,     KC_X,     KC_C,           KC_V,           KC_B,     KC_GRAVE,              CW_TOGG,          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSFT,
-    OSM(MOD_LCTL),  KC_TRNS,  KC_TRNS,  OSM(MOD_LALT),  OSM(MOD_LGUI),                                                                     TT(MOUSE),      TD(TD_UNDO),    TD(TD_COPY),    LGUI(KC_V),     LGUI(LSFT(KC_2)),
+    OSM(MOD_LCTL),  KC_TRNS,  KC_TRNS,  OSM(MOD_LALT),  OSM(MOD_LGUI),                                                                     TT(MOUSE),      TD(TD_UNDO),    TD(TD_COPY),    LGUI(KC_V),     CMD_TAB,
                                                                                   KC_TRNS,   KC_TRNS,    LALT(KC_SPACE),  LGUI(KC_SPACE),
-                                                                                          TG(GAMING),    KC_TRNS, 
+                                                                                          TG(GAMING),     LGUI(LSFT(KC_2)), 
                                                                     KC_SPACE, KC_DELETE, TT(NUMBERS),    LGUI(KC_GRAVE), KC_ENTER, KC_BSPC
 
  ),
@@ -205,7 +205,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                                                RGB_TOG, RGB_MODE_PLAIN,     KC_TRNS, KC_TRNS,
                                                         TOGGLE_LAYER_COLOR,     KC_TRNS,
-                                      CMD_TAB, QK_LEAD, KC_TRNS,      KC_TRNS, KC_LCTL, KC_TRNS
+                                      KC_TRNS, QK_LEAD, KC_TRNS,      KC_TRNS, KC_LCTL, KC_TRNS
 ),
 /* Keymap 2: Mouse layer
  *
