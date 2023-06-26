b19ba8f41b79026dbb6a973d5e2f430f4e05a886
Nick Schneider <nickschneider101@gmail.com>
2023-06-26 14:00:47 -0700

    Replace Tap Dance dash with regular dash and plus

--- keyboards/ergodox_ez/keymaps/campasachamp/keymap.c
+++ keyboards/ergodox_ez/keymaps/campasachamp/keymap.c
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [BASE] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,      KC_1,     KC_2,     KC_3,           KC_4,           KC_5,     TD(TD_DASH),           TD(TD_PLUS),      KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSPC,
+    KC_ESCAPE,      KC_1,     KC_2,     KC_3,           KC_4,           KC_5,     KC_MINUS,              KC_EQUAL,         KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSPC,
     KC_TAB,         KC_Q,     KC_W,     KC_E,           KC_R,           KC_T,     TD(TD_LBRACKET),       TD(TD_RBRACKET),  KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           TD(TD_PIPE),
     MO(SYMBOLS),    KC_A,     KC_S,     KC_D,           KC_F,           KC_G,                                              KC_H,           KC_J,           KC_K,           KC_L,           KC_SEMICOLON,   KC_QUOTE,
     KC_LSFT,        KC_Z,     KC_X,     KC_C,           KC_V,           KC_B,     KC_GRAVE,              CW_TOGG,          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSFT,
@@ -344,30 +344,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 */
 // clang-format on
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) { // This will do most of the grunt work with the keycodes.
-    case CMD_TAB:
-        if (record->event.pressed) 
-        {
-            if (!is_cmd_tab_active) 
+
+    switch (keycode) { // This will do most of the grunt work with the keycodes.
+        case KC_ESC:
+            if (get_oneshot_mods() != 0)
             {
-                is_cmd_tab_active = true;
-                register_code(KC_LCMD);
+                clear_oneshot_mods();
             }
-            cmd_tab_timer = timer_read();
-            register_code(KC_TAB);
-        } 
-        else 
-        {
-            unregister_code(KC_TAB);
-        }
-        break;
-    case VRSN:
-        SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        return false;
-  }
-  return true;
+            break;
+        case CMD_TAB:
+            if (record->event.pressed) 
+            {
+                if (!is_cmd_tab_active) 
+                {
+                    is_cmd_tab_active = true;
+                    register_code(KC_LCMD);
+                }
+                cmd_tab_timer = timer_read();
+                register_code(KC_TAB);
+            } 
+            else 
+            {
+                unregister_code(KC_TAB);
+            }
+            break;
+        case VRSN:
+            SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+            return false;
+    }
+    return true;
 }
 
 void matrix_scan_user(void) {
