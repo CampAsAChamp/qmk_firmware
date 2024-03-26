b99bdc835cc8524bb3b7d2d11ce456d884c7ac6d
Nick Schneider <nickschneider101@gmail.com>
2024-03-26 11:49:44 -0700

    Sort DZRGB60 rules

--- keyboards/dztech/dz60rgb_ansi/v2/rules.mk
+++ keyboards/dztech/dz60rgb_ansi/v2/rules.mk
@@ -1,16 +1,15 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes          # Mouse keys
-EXTRAKEY_ENABLE = yes          # Audio control and System control
-CONSOLE_ENABLE = no            # Console for debug
-COMMAND_ENABLE = no            # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no           # Enable keyboard RGB underglow
 AUDIO_ENABLE = no              # Audio output
-RGB_MATRIX_ENABLE = yes        # Use RGB matrix
-NO_USB_STARTUP_CHECK = no      # Disable initialization only when usb is plugged in
-
+BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+COMMAND_ENABLE = no            # Commands for debug and configuration
+CONSOLE_ENABLE = no            # Console for debug
+EXTRAKEY_ENABLE = yes          # Audio control and System control
 LTO_ENABLE = yes
+MOUSEKEY_ENABLE = yes          # Mouse keys
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+NO_USB_STARTUP_CHECK = no      # Disable initialization only when usb is plugged in
+RGB_MATRIX_ENABLE = yes        # Use RGB matrix
+RGBLIGHT_ENABLE = no           # Enable keyboard RGB underglow
