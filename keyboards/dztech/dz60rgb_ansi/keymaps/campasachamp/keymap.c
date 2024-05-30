0cce7ac7588177808f948b6212fc8a407b82ecc8
Nick Schneider <nickschneider101@gmail.com>
2024-05-30 10:14:08 -0700

    Add home row mods to DZ60

--- keyboards/dztech/dz60rgb_ansi/keymaps/campasachamp/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/campasachamp/keymap.c
@@ -107,9 +107,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_LAYER] = LAYOUT_60_ansi(
         QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-        MO(CAPS_LAYER), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+        MO(CAPS_LAYER), LCTL_T(KC_A), LCMD_T(KC_S), LSFT_T(KC_D), LOPT_T(KC_F), KC_G, KC_H, LOPT_T(KC_J), RSFT_T(KC_K), LCMD_T(KC_L), LCTL_T(KC_SCLN), KC_QUOT, KC_ENT,
         SC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SC_RSPC,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, QK_LEAD, LT(FN_LAYER, KC_APP), KC_RCTL),
+        KC_LCTL, KC_LGUI, MO(CAPS_LAYER), KC_SPC, KC_RALT, QK_LEAD, LT(FN_LAYER, KC_APP), KC_RCTL),
+
     /*
    * |----------------------------------------------------------------------------------------------------------------------------------------------------|
    * | Esc                      | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10  | F11  | F12  | Backsp                                       |
@@ -125,10 +126,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
     [CAPS_LAYER] = LAYOUT_60_ansi(
         QK_GESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_LBRC, KC_RBRC, KC_HOME, KC_UP, KC_END, KC_PSCR, KC_PGUP, KC_PGDN, _______,
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_GRAVE, _______,
-        _______, _______, _______, KC_CALC, _______, _______, KC_MINS, KC_UNDS, _______, _______, _______, CW_TOGG,
+        LOPT(KC_TAB), LOPT(KC_Q), LOPT(KC_W), LOPT(KC_E), LOPT(KC_R), KC_LBRC, KC_RBRC, KC_HOME, KC_UP, KC_END, KC_PSCR, KC_PGUP, KC_PGDN, _______,
+        _______, LOPT(KC_A), LOPT(KC_S), LOPT(KC_D), LOPT(KC_F), KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_GRAVE, _______,
+        _______, LOPT(KC_Z), LOPT(KC_X), LOPT(KC_C), LOPT(KC_V), LOPT(KC_B), KC_MINS, KC_UNDS, _______, _______, _______, _______,
         _______, _______, _______, CW_TOGG, _______, _______, _______, _______),
+
     /*
    * |--------------------------------------------------------------------------------------------------------------------------------------------------------------------|
    * |       |     |         |        |       |         |       |          |      |       |         |        |         |                                                  |
@@ -144,8 +146,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
     [FN_LAYER] = LAYOUT_60_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, RGB_TOG, _______, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, QK_BOOT,
-        _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______, RGB_M_P,
-        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_CAPS,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,
+        _______, _______, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, RGB_M_P,
+        KC_CAPS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, KC_CAPS,
         AG_TOGG, _______, _______, _______, _______, _______, _______, _______)
 };
