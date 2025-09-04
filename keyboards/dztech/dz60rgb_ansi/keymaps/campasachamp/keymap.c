1e7df38a652518d0b9e8c8cedf8de996a52d9692
Nick Schneider <nickschneider101@gmail.com>
2025-09-04 11:15:46 -0700

    Add braces/symbol layer to DZ60 to match other keyboards

--- keyboards/dztech/dz60rgb_ansi/keymaps/campasachamp/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/campasachamp/keymap.c
@@ -19,8 +19,9 @@
 
 enum layers {
     BASE_LAYER,
-    FN_LAYER,
-    CAPS_LAYER,
+    SHORTCUT_LAYER,
+    SYMBOL_LAYER,
+    MEDIA_LAYER,
     RGB_LAYER,
     MOUSE_LAYER
 };
@@ -105,7 +106,14 @@ void dance_r_bracket(tap_dance_state_t *state, void *user_data) {
 
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for first key code tap twice for second keycode
-    [TD_DASH] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS, KC_UNDS), [TD_PLUS] = ACTION_TAP_DANCE_DOUBLE(KC_EQUAL, KC_PLUS), [TD_LBRACKET] = ACTION_TAP_DANCE_FN(dance_l_bracket), [TD_RBRACKET] = ACTION_TAP_DANCE_FN(dance_r_bracket), [TD_PIPE] = ACTION_TAP_DANCE_DOUBLE(KC_BACKSLASH, KC_PIPE), [TD_COPY] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_C), LGUI(KC_X)), [TD_UNDO] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_Z), LGUI(LSFT(KC_Z))), [TD_CAPS] = ACTION_TAP_DANCE_FN(dance_caps),
+    [TD_DASH] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS, KC_UNDS), 
+    [TD_PLUS] = ACTION_TAP_DANCE_DOUBLE(KC_EQUAL, KC_PLUS), 
+    [TD_LBRACKET] = ACTION_TAP_DANCE_FN(dance_l_bracket), 
+    [TD_RBRACKET] = ACTION_TAP_DANCE_FN(dance_r_bracket), 
+    [TD_PIPE] = ACTION_TAP_DANCE_DOUBLE(KC_BACKSLASH, KC_PIPE), 
+    [TD_COPY] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_C), LGUI(KC_X)), 
+    [TD_UNDO] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_Z), LGUI(LSFT(KC_Z))), 
+    [TD_CAPS] = ACTION_TAP_DANCE_FN(dance_caps),
 };
 
 bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
@@ -156,21 +164,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_LAYER] = LAYOUT_60_ansi(
         QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-        LT(CAPS_LAYER, KC_ESC), LCMD_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), LCTL_T(KC_K), LOPT_T(KC_L), LCMD_T(KC_SCLN), KC_QUOT, KC_ENT,
-        TD(TD_CAPS), MY_MEH_Z, MY_HYPER_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RCTL, KC_SLASH), TD(TD_CAPS),
-        KC_LALT, KC_LWIN, KC_LCTL, LT(FN_LAYER, KC_SPC), MO(MOUSE_LAYER), LT(RGB_LAYER, KC_APP), _______, KC_RCTL),
+        LT(MEDIA_LAYER, KC_ESC), LCMD_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), LCTL_T(KC_K), LOPT_T(KC_L), LCMD_T(KC_SCLN), KC_QUOT, KC_ENT,
+        TD(TD_CAPS), MY_MEH_Z, MY_HYPER_X, KC_C, KC_V, KC_B, KC_N, KC_M, LT(SYMBOL_LAYER, KC_COMMA), MY_HYPER_DOT, MT(MOD_RCTL, KC_SLASH), TD(TD_CAPS),
+        KC_LALT, KC_LWIN, KC_LCTL, LT(SHORTCUT_LAYER, KC_SPC), MO(MOUSE_LAYER), LT(RGB_LAYER, KC_APP), _______, KC_RCTL),
 
-    [FN_LAYER] = LAYOUT_60_ansi(
+    [SHORTCUT_LAYER] = LAYOUT_60_ansi(
         LCTL(KC_GRV), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, LALT(KC_BSPC),
-        SUPER_ALT_TAB, LCTL(KC_Q), LCTL(KC_W), LCTL(KC_E), LCTL(KC_R), KC_LBRC, KC_RBRC, KC_HOME, KC_UP, KC_END, KC_PSCR, KC_PGUP, KC_PGDN, _______,
-        _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_GRAVE, LCTL(KC_ENT),
-        _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_B), KC_MINS, KC_UNDS, LCTL(KC_COMM), _______, LCTL(KC_SLASH), _______,
-        _______, _______, _______, LCTL(KC_SPC), _______, _______, _______, _______),
+        SUPER_ALT_TAB, LCTL(KC_Q), LCTL(KC_W), LCTL(KC_E), LCTL(KC_R), _______, _______, KC_HOME, KC_UP, KC_END, KC_PSCR, KC_PGUP, KC_PGDN, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_GRAVE, LCTL(KC_ENT),
+        _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_B), KC_MINS, KC_UNDS, LCTL(KC_COMM), _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______),
 
-    [CAPS_LAYER] = LAYOUT_60_ansi(
+    [SYMBOL_LAYER] = LAYOUT_60_ansi(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______),
+
+    [MEDIA_LAYER] = LAYOUT_60_ansi(
         KC_TILD, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_LBRC, KC_RBRC, _______, _______, _______, KC_PSCR, KC_PGUP, KC_PGDN, _______,
-        _______, KC_VOLD, KC_VOLU, _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______, _______, _______, _______,
+        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_PGUP, KC_PGDN, _______,
+        _______, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______),
         
@@ -188,3 +203,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, KC_MS_BTN2, KC_MS_BTN1, _______, _______, _______, _______)
 };
+
+
+/*
+BLANK_LAYER_TEMPLATE
+----------------------
+    [REPLACE_ME_LAYER] = LAYOUT_60_ansi(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______),
+
+*/
