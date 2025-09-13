9f798e1b3b57ec4adcc6bb3512e4c72626418640
Nick Schneider <nickschneider101@gmail.com>
2025-09-13 14:13:06 -0700

    Switch caps back

--- keyboards/ergodox_ez/glow/keymaps/campasachamp/keymap.c
+++ keyboards/ergodox_ez/glow/keymaps/campasachamp/keymap.c
@@ -46,7 +46,8 @@ enum td_keycodes {
     TD_PIPE,
     TD_COPY,
     TD_UNDO,
-    TD_CAPS,
+    TD_CAPS_BASIC,
+    TD_CAPS_ADVANCED,
     X_CTL,
 };
 
@@ -132,7 +133,8 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_PIPE] = ACTION_TAP_DANCE_DOUBLE(KC_BACKSLASH, KC_PIPE), 
     [TD_COPY] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_C), LGUI(KC_X)), 
     [TD_UNDO] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_Z), LGUI(LSFT(KC_Z))), 
-    [TD_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_caps_finished, dance_caps_reset)
+    [TD_CAPS_BASIC] = ACTION_TAP_DANCE_FN(dance_caps),
+    [TD_CAPS_ADVANCED] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_caps_finished, dance_caps_reset)
 
 };
 
@@ -174,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MINUS,         KC_1,         KC_2,         KC_3,         KC_4,           KC_5,         _______,      _______            , KC_6       , KC_7        , KC_8        , KC_9        , KC_0                , KC_EQUAL    ,
   KC_TAB,         KC_Q,         KC_W,         KC_E,         KC_R,           KC_T,  _______,      _______     , KC_Y       , KC_U        , KC_I        , KC_O        , KC_P                , TD(TD_PIPE),
   LT(MEDIA, KC_ESC), LCMD_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F),           KC_G,                                              KC_H       , RSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LCMD_T(KC_SEMICOLON), KC_QUOTE   ,
-  TD(TD_CAPS) ,         MY_MEH_Z,         MY_HYPER_X,          LT(SYMBOLS, KC_C),        KC_V,           KC_B,   LCTL(KC_SPACE),      TD(TD_CAPS)         , KC_N       , KC_M        , LT(SYMBOLS, KC_COMMA)    , MY_HYPER_DOT      , MT(MOD_RCTL, KC_SLASH)            , TD(TD_CAPS)     ,
+  TD(TD_CAPS_BASIC) ,         MY_MEH_Z,         MY_HYPER_X,          LT(SYMBOLS, KC_C),        KC_V,           KC_B,   LCTL(KC_SPACE),      _______         , KC_N       , KC_M        , LT(SYMBOLS, KC_COMMA)    , MY_HYPER_DOT      , MT(MOD_RCTL, KC_SLASH)            , TD(TD_CAPS_BASIC)     ,
   TOGGLE_LAYER_COLOR,       _______,      _______, _______,  MO(SHORTCUTS),                                                                           TT(MOUSE)   , _______     , _______     , _______             , _______    ,
 
                                                                    LALT(KC_SPACE), LCTL(LSFT(KC_1)),      TG(GAMING)          , TT(NUMBERS),
@@ -186,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      LCTL(KC_GRV),      KC_F1,      KC_F2,      KC_F3,      KC_F4,   KC_F5,  KC_F11,      KC_F12 , KC_F6         , KC_F7    , KC_F8  , KC_F9   , KC_F10        , TO(BASE),
      SUPER_ALT_TAB, LCTL(KC_Q), LCTL(KC_W),    _______,    _______, KC_LBRC, _______,      _______, KC_RBRC       , KC_HOME  , KC_UP  , KC_END  , _______       , _______ ,
      LCTL(KC_GRV),    _______,    _______,    _______,    _______, KC_LCBR,                        KC_RCBR       , KC_LEFT  , KC_DOWN, KC_RIGHT, _______       , KC_GRAVE,
-     _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______, KC_CAPS,      TD(TD_CAPS), KC_MINS       , KC_UNDS  , _______, _______ , _______, _______ ,
+     _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______, KC_CAPS,      TD(TD_CAPS_BASIC), KC_MINS       , KC_UNDS  , _______, _______ , _______, _______ ,
      _______,    _______,    _______,    _______,    _______,                                                 _______  , _______, _______ , _______       , _______ ,
 
                                                               _______, _______,      _______, _______       ,
