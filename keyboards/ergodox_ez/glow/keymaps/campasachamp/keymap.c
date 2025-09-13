cd9878d5d01b32b10398d134e135ca5a7ed2e34c
Nick Schneider <nickschneider101@gmail.com>
2025-09-13 14:04:07 -0700

    Add hold for shift keys

--- keyboards/ergodox_ez/glow/keymaps/campasachamp/keymap.c
+++ keyboards/ergodox_ez/glow/keymaps/campasachamp/keymap.c
@@ -21,6 +21,23 @@ enum layers {
 };
 
 // Tap Dance index enum declarations
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP, // Send two single taps
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
 enum td_keycodes {
     TD_DASH,
     TD_PLUS,
@@ -30,6 +47,7 @@ enum td_keycodes {
     TD_COPY,
     TD_UNDO,
     TD_CAPS,
+    X_CTL,
 };
 
 enum custom_keycodes {
@@ -37,6 +55,13 @@ enum custom_keycodes {
     RGB_SLD,
     COLON_EQUAL,
 };
+
+// State detection helper
+td_state_t cur_dance(tap_dance_state_t *state);
+
+// For the x tap dance. Put it here so it can be used in any keymap
+void dance_caps_finished(tap_dance_state_t *state, void *user_data);
+void dance_caps_reset(tap_dance_state_t *state, void *user_data);
 
 void dance_caps(tap_dance_state_t *state, void *user_data) {
     if (state->count == 1) {
@@ -100,7 +125,15 @@ void dance_r_bracket(tap_dance_state_t *state, void *user_data) {
 
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for first key code tap twice for second keycode
-    [TD_DASH] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS, KC_UNDS), [TD_PLUS] = ACTION_TAP_DANCE_DOUBLE(KC_EQUAL, KC_PLUS), [TD_LBRACKET] = ACTION_TAP_DANCE_FN(dance_l_bracket), [TD_RBRACKET] = ACTION_TAP_DANCE_FN(dance_r_bracket), [TD_PIPE] = ACTION_TAP_DANCE_DOUBLE(KC_BACKSLASH, KC_PIPE), [TD_COPY] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_C), LGUI(KC_X)), [TD_UNDO] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_Z), LGUI(LSFT(KC_Z))), [TD_CAPS] = ACTION_TAP_DANCE_FN(dance_caps),
+    [TD_DASH] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS, KC_UNDS), 
+    [TD_PLUS] = ACTION_TAP_DANCE_DOUBLE(KC_EQUAL, KC_PLUS), 
+    [TD_LBRACKET] = ACTION_TAP_DANCE_FN(dance_l_bracket), 
+    [TD_RBRACKET] = ACTION_TAP_DANCE_FN(dance_r_bracket), 
+    [TD_PIPE] = ACTION_TAP_DANCE_DOUBLE(KC_BACKSLASH, KC_PIPE), 
+    [TD_COPY] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_C), LGUI(KC_X)), 
+    [TD_UNDO] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_Z), LGUI(LSFT(KC_Z))), 
+    [TD_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_caps_finished, dance_caps_reset)
+
 };
 
 const uint16_t PROGMEM HOMEROW_APP_ACTIVATION[] = {KC_E, KC_I, COMBO_END};
@@ -180,7 +213,7 @@ _______, _______, KC_VOLD,  KC_VOLU, _______, _______,                          
 _______, _______, _______, _______,  _______, _______,            _______,      _______, _______, _______, _______, _______, _______, _______ ,
 _______, _______, _______, _______, _______,                                                     _______, _______, _______, _______, _______ ,
 
-                                              RGB_TOG,     RGB_MODE_PLAIN,      _______, _______,
+                                              _______,     RGB_MODE_PLAIN,      _______, _______,
                                                        TOGGLE_LAYER_COLOR,      _______,
                                      _______, _______,            _______,      _______, _______, _______
 ),
@@ -198,15 +231,15 @@ _______, _______, _______, _______,  _______,                                   
 ),
 
 [MOUSE] = LAYOUT_ergodox_pretty(
-_______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______,
-_______, _______, KC_WH_U, KC_MS_U, KC_WH_D, _______, _______,      _______, _______, KC_WH_U, KC_MS_U, KC_WH_D, _______, QK_BOOT,
-_______, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,                        KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, _______,
-KC_BTN4, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, KC_BTN5,
-_______, _______, _______, _______, KC_BTN1,                                          _______, _______, _______, _______, _______,
+_______, MS_ACL0, MS_ACL1, MS_ACL2, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______,
+_______, _______, MS_WHLU, MS_UP, MS_WHLD, _______, _______,      _______, _______, _______, _______, _______, _______, QK_BOOT,
+_______, MS_WHLL, MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLL,                        _______, _______, _______, _______, _______, _______,
+MS_BTN4, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, MS_BTN5,
+_______, _______, _______, _______, MS_BTN1,                                          _______, _______, _______, _______, _______,
 
                                              _______, _______,      _______, _______,
                                                       _______,      _______,
-                                    KC_BTN2, KC_BTN3, _______,      _______, _______, _______
+                                    MS_BTN2, MS_BTN3, _______,      _______, _______, _______
 ),
 
 [GAMING] = LAYOUT_ergodox_pretty(
@@ -526,3 +559,78 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
     return state;
 };
+
+/* Return an integer that corresponds to what kind of tap dance should be executed.
+ *
+ * How to figure out tap dance state: interrupted and pressed.
+ *
+ * Interrupted: If the state of a dance is "interrupted", that means that another key has been hit
+ *  under the tapping term. This is typically indicative that you are trying to "tap" the key.
+ *
+ * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
+ *  has ended, but the key is still being pressed down. This generally means the key is being "held".
+ *
+ * One thing that is currently not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
+ *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
+ *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
+ *
+ * Good places to put an advanced tap dance:
+ *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
+ *
+ * Criteria for "good placement" of a tap dance key:
+ *  Not a key that is hit frequently in a sentence
+ *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
+ *    in a web form. So 'tab' would be a poor choice for a tap dance.
+ *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
+ *    letter 'p', the word 'pepper' would be quite frustrating to type.
+ *
+ * For the third point, there does exist the 'TD_DOUBLE_SINGLE_TAP', however this is not fully tested
+ *
+ */
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+        // keystrokes of the key, and not the 'double tap' action/macro.
+        if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return TD_DOUBLE_HOLD;
+        else return TD_DOUBLE_TAP;
+    }
+
+    // Assumes no one is trying to type the same letter three times (at least not quickly).
+    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+    // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
+        else return TD_TRIPLE_HOLD;
+    } else return TD_UNKNOWN;
+}
+
+// Create an instance of 'td_tap_t' for the 'x' tap dance.
+static td_tap_t xtap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+void dance_caps_finished(tap_dance_state_t *state, void *user_data) {
+    xtap_state.state = cur_dance(state);
+    switch (xtap_state.state) {
+        case TD_SINGLE_TAP:  set_oneshot_mods(MOD_BIT(KC_LSFT)); break;
+        case TD_SINGLE_HOLD: register_code(KC_LSFT); break;
+        case TD_DOUBLE_TAP:  caps_word_on(); break;
+        case TD_TRIPLE_TAP:  tap_code(KC_CAPS); break;
+        default: break;
+    }
+}
+
+void dance_caps_reset(tap_dance_state_t *state, void *user_data) {
+    switch (xtap_state.state) {
+        case TD_SINGLE_HOLD: unregister_code(KC_LSFT); break;
+        default: break;
+    }
+    xtap_state.state = TD_NONE;
+}
